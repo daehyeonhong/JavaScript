3a7c4d26a75a88c628ca53adb15a9623cb47eda6
DaeHyeon-Hong <fholng@outlook.com>
2020-06-19 12:48:07 +0900

    no message

--- JavaScript/WebContent/WEB-INF/variable.js
+++ JavaScript/WebContent/WEB-INF/variable.js
@@ -3,7 +3,7 @@
 // use this for Vanila Javascript.
 'use strict';
 
-// 2.Variable, rw(read/write)
+// 2.Variable
 // let (added in ES6)
 let globalName = 'global name';
 {
@@ -24,21 +24,14 @@ console.log(globalName);
 }
 console.log(age);
 
-// 3. Constant, r(read only)
-//--상수 - immutable
+// 3. Constants--상수 - immutable
 // let - mutable
 // favor immutable data type always for a few reasons;
 // - security
 // - thread safety
 // - reduce human mistakes
 const dayInWeek = 7; const maxNumber = 5;
-// Note!
-// Immutable data types: premitive types, frozen objects (i.e. object.freeze())
-// Mutable data types: all objects by default are mutable in JS
-// favor immutable data type always for a few reasons:
-// - security
-// - thread safety
-// - reduce human mistakes
+
 // 4. Variable types
 // Primitive, single item; number, string, boolean, null, undefiend, symbol
 // object, box container
@@ -67,38 +60,16 @@ console.log(`value: ${helloBob}, type: ${typeof hellBob}`);
 const canRead = true;
 const test = 3 < 1;// false
 console.log(`value: ${canRead}, type: ${typeof canRead}`);
-console.log(`value: ${test}, type: ${typeof test}`);
+console.log(`value: ${test}, type: ${testof test}`);
 
 // null
 let nothing = null;
-console.log(`value: ${nothing}, type: ${typeof nothing}`);
+console.log(`value: ${ nothing }, type: ${ typeof nothing }`);
 
 // undefined
 let x;
-console.log(`value: ${x}, type: ${typeof x}`);
+console.log(`value: ${ x }, type: ${ typeof x }`);
 
 // symbol, create unique identifiers for objects
 const symbol1 = Symbol('id');
 const symbol2 = Symbol('id');
-console.log(symbol1 === symbol2);
-const gSymbol1 = Symbol.for('id');
-const gSymbol2 = Symbol.for('id');
-console.log(gSymbol1 === gSymbol2);
-console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
-
-// 5. Dynamic typing: dynamically typed language
-let text = 'hello';
-console.log(text.charAt(0));
-console.log(`value: ${text}, type: ${typeof text}`);
-text = 1;
-console.log(`value: ${test}, type: ${typeof text}`);
-text = '7' + 5;
-console.log(`value: ${text}, type: ${typeof text}`);
-text = '8' / '2';
-console.log(`value: &{text}, type: ${typeof text}`);
-//console.log(text.charAt(0));
-
-// object, real-life object, data structure
-const hong = { name: 'hong', age: 20 };
-hong.age = 21;
-console.log(`value: ${text}, type: ${typeof text}`);
