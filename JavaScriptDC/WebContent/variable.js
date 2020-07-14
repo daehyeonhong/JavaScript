1b60bc8adab48c52490598be4497d5cc4514d573
DaeHyeon-Hong <fholng@outlook.com>
2020-07-14 09:37:49 +0900

    no message

---
+++ JavaScriptDC/WebContent/variable.js
@@ -0,0 +1,12 @@
+/* 1. Use strict
+added in ES 5
+use this for Vanila JavaScript */
+"use scrict";
+
+/* 2. Variable
+let (added in ES6) */
+
+let name = "ellie";
+console.log(name);
+name = "hello";
+console.log(name);
