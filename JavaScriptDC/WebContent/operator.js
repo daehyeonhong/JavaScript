e1d20f49302b59989dfb1570c5a955a0e840b939
DaeHyeon-Hong <fholng@outlook.com>
2020-07-15 09:08:33 +0900

    ㅁㅁ

--- JavaScriptDC/WebContent/operator.js
+++ JavaScriptDC/WebContent/operator.js
@@ -1,6 +1,6 @@
 /* 1. String concatenation */
-console.log("my" + " cat");
-console.log("1" + 2);
+console.log('my' + ' cat');
+console.log('1' + 2);
 console.log(`string literals: 1+2 = ${1 + 2}`);
 
 /* 2. Numeric operators */
@@ -57,7 +57,7 @@ nullAbleObject && nullAbleObject.something */
 function check() {
   for (let i = 0; i < 10; i++) {
     /* wasting time */
-    console.log("😱");
+    console.log('😱');
   }
   return true;
 }
@@ -66,7 +66,7 @@ function check() {
 console.log(!value1);
 
 /* 7. Equality */
-const stringFive = "5";
+const stringFive = '5';
 const numberFive = 5;
 
 /* == loose equality, with type conversion */
@@ -78,8 +78,8 @@ console.log(stringFive === numberFive);
 console.log(stringFive !== numberFive);
 
 /* object equality by reference */
-const hong1 = { name: "hong" };
-const hong2 = { name: "hong" };
+const hong1 = { name: 'hong' };
+const hong2 = { name: 'hong' };
 const hong3 = hong1;
 console.log(hong1 == hong2);
 console.log(hong1 === hong2);
@@ -88,43 +88,43 @@ console.log(hong1 === hong3);
 /* equlity - puzzler */
 console.log(0 == false); /* true - o */
 console.log(0 === false); /* false - o */
-console.log("" == false); /* true - o */
-console.log("" === false); /* false - o */
+console.log('' == false); /* true - o */
+console.log('' === false); /* false - o */
 console.log(null == undefined); /* true - o */
 console.log(null === undefined); /* false - o */
 
 /* 8. Conditional operators: if
 if, else if, else */
-const name = "hong";
-if (name === "hong") {
+const name = 'hong';
+if (name === 'hong') {
   console.log(`Welecome ${name}!`);
-} else if (name === "coder") {
+} else if (name === 'coder') {
   console.log(`You are amazing coder`);
 } else {
-  console.log("unknown");
+  console.log('unknown');
 }
 
 /* 9. Ternary operator: ?
 condition ? value1 : value2; */
-console.log(name === "hong" ? "yes" : "no");
+console.log(name === 'hong' ? 'yes' : 'no');
 
 /*10. Switch statement
 use for multiple if checks
 use for enum-like value check
 use for multiple type checks in TS*/
-const browser = "IE";
+const browser = 'IE';
 switch (browser) {
-  case "IE":
-    console.log("go away!");
+  case 'IE':
+    console.log('go away!');
     break;
-  case "Chrome":
-    console.log("Chrome");
+  case 'Chrome':
+    console.log('Chrome');
     break;
-  case "Firefox":
-    console.log("Firefox");
+  case 'Firefox':
+    console.log('Firefox');
     break;
   default:
-    console.log("same all!");
+    console.log('same all!');
     break;
 }
 
@@ -164,18 +164,18 @@ for (let i = 0; i < 10; i++) {
 /*break, continue*/
 /* Q1. iterate from 0 to 10 and print only even numbers(use continue) */
 for (let h = 0; h <= 10; h++) {
-    if (h%2==0) {
-        console.log(h);
-        console.log(`Q1. ${h}`);
-    }else{
-        continue;
-    }
+  if (h % 2 == 0) {
+    console.log(h);
+    console.log(`Q1. ${h}`);
+  } else {
+    continue;
+  }
 }
 /* Q2. iterate from 0 to 10 and print numbers until reaching 8 (use break) */
 for (let h = 0; h <= 10; h++) {
-    if (h>8) {
-        break;
-    }
-    console.log(h);
-    console.log(`Q2. ${h}`);
+  if (h > 8) {
+    break;
+  }
+  console.log(h);
+  console.log(`Q2. ${h}`);
 }
