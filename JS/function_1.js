df4f1ddf5bda8582afa5fba6279bc698b9365a5b
Daehyeon Hong <fholng@outlook.com>
2021-01-10 17:31:57 +0900

    JavaScript Function

---
+++ JS/function_1.js
@@ -0,0 +1,21 @@
+if (10 === 5) {
+  console.log('hi');
+} else {
+  console.log('ho');
+}
+
+if (20 > 5 && 'a' === 'a') {
+  console.log('yes');
+} else {
+  console.log('no');
+}
+
+const age = prompt('How old are you?');
+
+if (age >= 18 && age <= 21) {
+  console.log('you can drink but you should not');
+} else if (age > 21) {
+  console.log('ao ahead');
+} else {
+  console.log('too young');
+}
