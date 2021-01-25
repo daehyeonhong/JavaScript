a305039696403f4023e6a5c676048fc6fa7c4059
DaeHyeonHong <fholng@outlook.com>
2021-01-25 23:53:15 +0900

    JavaScript toggle

--- JS/function_2.js
+++ JS/function_2.js
@@ -1,20 +1,27 @@
 const title = document.querySelector('#title');
 
-const BASE_COLOR = 'rgb(52,73,94)';
-const OTHER_COLOR = '#123123';
+const BASE_COLOR = 'rgb(52, 73, 94)';
+const OTHER_COLOR = '#7f8c8d';
 
 function handleClick() {
   const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
+  title.style.color = currentColor === BASE_COLOR ? OTHER_COLOR : BASE_COLOR;
 }
 
 function init() {
   title.style.color = BASE_COLOR;
-  title.addEventListener('click', handleClick);
+  title.addEventListener('mouseenter', handleClick);
 }
 
 init();
+
+function handdleOfflie() {
+  console.log('Bye bye');
+}
+
+function handleOnline() {
+  console.log('Welcome Back');
+}
+
+window.addEventListener('offline', handleOffline);
+window.addEventListener('online', handleOnline);
