7e5f271db822c245ded0a6ef8d53e860826a77d2
DaeHyeonHong <fholng@outlook.com>
2021-02-07 02:37:20 +0900

    Learn JavaScript Variable && Operator

--- JavaScriptDC/JavaScript/variable.js
+++ JavaScriptDC/JavaScript/variable.js
@@ -1,17 +1,17 @@
 //  1. Use Strict
 'use strict';
 
-// 2. Variable
+// 2. Variable, rw(read/write)
 // Mutable
 // let (added in ES6)
 
 let globalName = 'Global Name';
 
 {
-  let name = 'hong';
-  console.log(name);
-  name = 'hello';
-  console.log(name);
+	let name = 'hong';
+	console.log(name);
+	name = 'hello';
+	console.log(name);
 }
 console.log(globalName);
 
@@ -21,13 +21,19 @@ console.log(globalName);
 
 console.log(age);
 {
-  age = 4;
-  console.log(age);
-  var age;
+	age = 4;
+	console.log(age);
+	var age;
 }
 console.log(age);
 
-// 3. Constants
+// 3. Constant, r(read only)
+// use const whenever possible.
+// only use let if variable needs to change.
+
+// Note!
+// Immutable data types: primitive types, frozen object (i.e object.freeze())
+// Mutable data types: all objects by default are mutable in JS
 // favor immutable data type always for a few reasons;
 // - security
 // - thread safety
@@ -68,3 +74,47 @@ const greeting = 'Hello, ' + brenden;
 console.log(`value: ${greeting}, type: ${typeof greeting}`);
 const helloBob = `Hi ${brenden}!`; // template literals (string)
 console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+/* console.log('value: ' + helloBob + ', type: ' + typeof helloBob); */
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1;// false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// simbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); // true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+
+// object, real-life object, data structure
+const man = { name: 'man', age: 30 };
+man.age = 21;
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); // h
+console.log(`value: ${text}, type: ${typeof text}`);
+/* console.log(`value: ${}, type: ${typeof }`); */
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
+console.log(text.charAt(0)); // Uncaught TypeError
